8bce64f5dac36c310c987770147047d9fbc783c8
Kouki Shu <shukouki@gmail.com>
2020-10-25 22:56:10 +0900

    Add a file

---
+++ abc162b-FizzBuzzSum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+	int n;
+	cin>>n;
+	long long s=0;
+	  for(int i=1;i<n+1;i++){
+		 if((i%3==0){
+		 }
+		 else if(i%5==0){
+		 }
+		 else
+		   s += i;
+	   }
+	   cout << s << endl;
+}
