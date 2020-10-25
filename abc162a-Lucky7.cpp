12e3ff2785a1aa715050605cb265397e03caf4c2
Kouki Shu <shukouki@gmail.com>
2020-10-25 22:42:41 +0900

    Add a file

---
+++ abc162a-Lucky7.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	  string a;
+	    cin>>a;
+	      if(a.at(0)=='7'||a.at(1)=='7'||a.at(2)=='7'){
+		          cout << "Yes" <<endl;
+			    }
+	        else
+			    cout << "No"<<endl;
+}
